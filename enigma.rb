e6d2eb917e05e4b8f4815a601a30d15b4f36460e
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-02 13:46:51 +0100

    pursued the work

--- enigma.rb
+++ enigma.rb
@@ -1,25 +1,51 @@
 operation = gets.chomp
 pseudo_random_number = gets.to_i
 rotors = []
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
-3.times do |i|
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.freeze
+
+3.times do
   rotor = gets.chomp
-  rotors[i] = rotor.split('')
+  rotors << rotor
 end
 message = gets.chomp
 
-case operation
-when 'ENCODE'
-    puts message
-when 'DECODE'
-    puts "DECODE"
+def encode(message, number, rotors)
+  # stocker les caractères dans un array => 'message_arr'
+  message_arr = message.chars
+  # étape 1 : Caesar shift | message_arr + nb => array 'message_shifted'
+  message_shifted = caesar(message_arr, number)
+  # étapes 2, 3 et 4 : ROTOR shift | message_shifted + rotors => array 'message_rotored'
+  message1 = rotoring(message_shifted, rotors[0])
+  message2 = rotoring(message1, rotors[1])
+  message_rotored = rotoring(message2, rotors[2])
+  # étape 5 : compiler dans une string | message_rotored => string
+  message_rotored.join
 end
 
-def encode
-  # stocker les caractères dans un array => 'message_arr'
-  # étape 1 : Caesar shift | message_arr + nb => array 'message_shifted'
-  # étapes 2, 3 et 4 : ROTOR shift | message_shifted + rotors => array 'message_rotored'
-  # étape 5 : compiler dans une string | message_rotored => string
+def caesar(array, number)
+  message_shifted = []
+  array.each_with_index do |letter, index|
+    former_index = ALPHABET.index(letter)
+    new_letter = ALPHABET[former_index + number + index]
+    message_shifted << new_letter
+  end
+  message_shifted
+end
+
+def rotoring(message, model)
+  new_message = []
+  message.each_with_index do |letter, index|
+    new_message << model[index]
+  end
+  new_message
+end
+
+case operation
+when 'ENCODE'
+  answer = encode(message, pseudo_random_number, rotors)
+  puts answer
+when 'DECODE'
+  puts 'DECODE'
 end
 
 # To debug: STDERR.puts "Debug messages..."
