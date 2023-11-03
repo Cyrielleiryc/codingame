533670ceaa5a5794b1c4c7aeb022b38525f55f29
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-03 12:40:33 +0100

    method AND

--- logicgates.rb
+++ logicgates.rb
@@ -10,11 +10,11 @@ n.times do
   # => inputs['A'] = "__---___---___---___---___"
 end
 
-outputs = {}
+outputs = []
 m.times do
   output_name, type, input_name_1, input_name_2 = gets.split
-  outputs[output_name] = [type, input_name_1, input_name_2]
-  # => outputs['C'] = ["AND", "A", "B"]
+  outputs << [output_name, type, input_name_1, input_name_2]
+  # => outputs[0] = ["C", "AND", "A", "B"]
 end
 
 # # # Code to implemente the answer # # #
@@ -25,12 +25,37 @@ def trueOrFalse(char)
   # '_' => false // '-' => true
 end
 
+def char(boolean)
+  return boolean ? "-" : "_"
+end
+
+def andmethod(one, two)
+  # prend 2 strings et compare chaque caractère un par un
+  new_string = " "
+  one.split('').each_with_index do |char, index|
+    boo1 = trueOrFalse(char)
+    boo2 = trueOrFalse(two[index])
+    new_string += char(boo1 && boo2)
+  end
+  new_string
+end
 
 # # # Code to print the answer  # # #
 # # # # # # # # # # # # # # # # # # #
-m.times do
-  puts "answer"
+
+outputs.each do |output|
+  answer = ""
+  case output[1]
+  when "AND"
+    answer += andmethod(inputs[output[2]], inputs[output[3]])
+  end
+  puts output[0] + answer
 end
+
+
+# m.times do
+#   puts "answer"
+# end
 # C ____-_____-_____-_____-___
 # D __-----_-----_-----_-----_
 # E __--_--_--_--_--_--_--_--_
