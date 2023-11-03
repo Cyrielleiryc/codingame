49278b46e3b418eaf0ca2a0b1da8674c628d793e
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-03 12:26:55 +0100

    organised the file

--- logicgates.rb
+++ logicgates.rb
@@ -1,9 +1,5 @@
-# _ => false
-# - => true
-
-def trueOrFalse(char)
-  return char == "-"
-end
+# # # Gets the info from the user # # #
+# # # # # # # # # # # # # # # # # # # #
 
 n = gets.to_i # 2
 m = gets.to_i # 3
@@ -11,27 +7,30 @@ inputs = {}
 n.times do
   input_name, input_signal = gets.split
   inputs[input_name] = input_signal
+  # => inputs['A'] = "__---___---___---___---___"
 end
-# A __---___---___---___---___
-# B ____---___---___---___---_
 
 outputs = {}
 m.times do
   output_name, type, input_name_1, input_name_2 = gets.split
   outputs[output_name] = [type, input_name_1, input_name_2]
+  # => outputs['C'] = ["AND", "A", "B"]
 end
-# C AND A B
-# D OR A B
-# E XOR A B
 
+# # # Code to implemente the answer # # #
+# # # # # # # # # # # # # # # # # # # # #
+
+def trueOrFalse(char)
+  return char == "-"
+  # '_' => false // '-' => true
+end
+
+
+# # # Code to print the answer  # # #
+# # # # # # # # # # # # # # # # # # #
 m.times do
   puts "answer"
 end
 # C ____-_____-_____-_____-___
 # D __-----_-----_-----_-----_
 # E __--_--_--_--_--_--_--_--_
-
-puts "inputs ="
-puts inputs['A']
-puts "outputs ="
-puts outputs['C'][0]
