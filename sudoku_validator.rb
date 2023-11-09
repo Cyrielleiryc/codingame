8efd0f32db6744060d4d87e0ff9de6bcd161cd8e
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-09 18:12:25 +0100

    SUCCESS for checking validity of lines and columns

--- sudoku_validator.rb
+++ sudoku_validator.rb
@@ -10,4 +10,27 @@ grid = []
   grid << line
 end
 
-puts grid[8].join('')
+# étape 1 : vérification des lignes
+def are_lines_ok(grid)
+  answer = true
+  grid.each do |line|
+    answer = false if line.uniq.length != line.length
+  end
+  answer
+end
+
+# étape 2 : vérification des colonnes en les transformant en lignes
+def are_columns_ok(grid)
+  new_grid = [[], [], [], [], [], [], [], [], []]
+  grid.each do |line|
+    i = 0
+    while i < line.length
+      new_grid[i] << line[i]
+      i += 1
+    end
+  end
+  are_lines_ok(new_grid)
+end
+
+
+puts are_lines_ok(grid) && are_columns_ok(grid)
