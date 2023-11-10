21c3de864caa4e379205f0d33e66780768b9ee03
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-10 17:07:33 +0100

    pseudo code for the game TODO: pseudo code for the dual method

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,43 +1,64 @@
 # getting game info from the user
 n = gets.to_i
-first_round = []
+number_of_rounds = Math.cbrt(n)
+players = []
 n.times do
   numplayer, signplayer = gets.split
   numplayer = numplayer.to_i
-  first_round << {numplayer => signplayer}
+  players << {numplayer => signplayer}
+end
+# # entrée
+  # 8 => nombre de joueurs
+  # 4 R => numéro du joueur + le coup qu'il joue
+  # 1 P
+  # 8 P
+  # 3 R
+  # 7 C
+  # 5 S
+  # 6 L
+  # 2 L
+
+# # pour chaque round
+  # grouper les joueurs par 2 => .each_slice(2).to_a
+  # faire un duel au sein de chaque array
+  # récupérer le vainqueur et le perdant
+  # enlever les perdants des tableaux et recommencer
+# # à la fin du jeu
+  # il ne reste qu'un joueur
+  
+
+
+# méthode pour dire qui a battu qui dans le duel
+  # => {vainqueur => [perdant]}           1er round
+  # => {vainqueur => [perdant, perdant]}  2e round etc
+def dual(array_of_two_players)
+
 end
 
-
-puts first_round[0][4]
-
-# # # test 1
-
-# # entrée
-
-# 8 => nombre de joueurs
-# 4 R => numéro du joueur + le coup qu'il joue
-# 1 P
-# 8 P
-# 3 R
-# 7 C
-# 5 S
-# 6 L
-# 2 L
-
+# réponse apportée
+puts first_round[0]
 # # sortie
+  # 2 => numéro du vainqueur
+  # 6 5 1 => adversaires du vainqueur dans l'ordre chronologique
 
-# 2 => numéro du vainqueur
-# 6 5 1 => adversaires du vainqueur dans l'ordre chronologique
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # # rules
-# Scissors cuts Paper
-# Paper covers Rock
-# Rock crushes Lizard
-# Lizard poisons Spock
-# Spock smashes Scissors
-# Scissors decapitates Lizard
-# Lizard eats Paper
-# Paper disproves Spock
-# Spock vaporizes Rock
-# Rock crushes Scissors
-# and in case of a tie, the player with the lowest number wins (it's scandalous but it's the rule).
+  # Scissors cuts Paper           C > P
+  # Paper covers Rock             P > R
+  # Rock crushes Lizard           R > L
+  # Lizard poisons Spock          L > S
+  # Spock smashes Scissors        S > C
+  # Scissors decapitates Lizard   C > L
+  # Lizard eats Paper             L > P
+  # Paper disproves Spock         P > S
+  # Spock vaporizes Rock          S > R
+  # Rock crushes Scissors         R > C
+  # and in case of a tie, the player with the lowest number wins
+
+# # code
+  # Rock (R)
+  # Paper (P)
+  # sCissors (C)
+  # Lizard (L)
+  # Spock (S)
