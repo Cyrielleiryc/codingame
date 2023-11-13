7961f88c3b4a836948922d9bde52be1605a43ea5
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-13 18:51:28 +0100

    methods to update arrays of players and winners are OK TODO: method to give the winner after one dual

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,12 +1,14 @@
 # getting game info from the user
 n = gets.to_i
-number_of_rounds = Math.cbrt(n)
 players = []
+winners = {}
 n.times do
   numplayer, signplayer = gets.split
   numplayer = numplayer.to_i
-  players << {numplayer => signplayer}
+  players << { name: numplayer, power: signplayer }
+  winners[numplayer] = []
 end
+
 # # entrée
   # 8 => nombre de joueurs
   # 4 R => numéro du joueur + le coup qu'il joue
@@ -18,25 +20,58 @@ end
   # 6 L
   # 2 L
 
+# méthode pour dire qui a battu qui dans le duel
+  # => {vainqueur => [perdant]}           1er round
+  # => {vainqueur => [perdant, perdant]}  2e round etc
+  def who_wins(array_of_two_players) # => donne le joueur vainqueur || exemple : {2 => "L"}
+    array_of_two_players[0]
+  end
+
+# méthode pour mettre à jour le tableau des joueurs
+# def update_players(loser, players)
+#   players.reject { |player| player == loser }
+# end
+def update_players(players, losers)
+  losers.each do |loser|
+    players.reject! { |player| player == loser }
+  end
+  players
+end
+
+# méthode pour mettre à jour le tableau des vainqueurs
+def update_winners(winner, loser, winners)
+  name_of_loser = loser[:name]
+  name_of_winner = winner[:name]
+  winners.delete(name_of_loser)
+  winners[name_of_winner] << name_of_loser
+  winners
+end
+
+
 # # pour chaque round
   # grouper les joueurs par 2 => .each_slice(2).to_a
   # faire un duel au sein de chaque array
   # récupérer le vainqueur et le perdant
-  # enlever les perdants des tableaux et recommencer
-# # à la fin du jeu
-  # il ne reste qu'un joueur
-  
+  # enlever les perdants des tableaux (players & winners) et recommencer => hash.delete(key)
+  # à la fin du jeu, il ne reste qu'un joueur
 
 
-# méthode pour dire qui a battu qui dans le duel
-  # => {vainqueur => [perdant]}           1er round
-  # => {vainqueur => [perdant, perdant]}  2e round etc
-def dual(array_of_two_players)
-
+while players.size != 1
+  rounds = players.each_slice(2).to_a
+  losers = []
+  rounds.each do |round|
+    winner = who_wins(round)
+    loser = round.reject! { |player| player == winner }.first
+    losers << loser
+    winners = update_winners(winner, loser, winners)
+  end
+  players = update_players(players, losers)
 end
 
+
 # réponse apportée
-puts first_round[0]
+puts winners
+
 # # sortie
   # 2 => numéro du vainqueur
   # 6 5 1 => adversaires du vainqueur dans l'ordre chronologique
