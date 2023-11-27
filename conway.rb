9b911081480f8fc309d43ac1761e9e7705a168ee
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-27 14:32:40 +0100

    started new challenge, difficulty +

---
+++ conway.rb
@@ -0,0 +1,10 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+r = gets.to_i
+l = gets.to_i
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "answer"
