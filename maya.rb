4f3aa6a55a3d72b6cce6f8f8b9239094a54bf6eb
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-11-27 15:56:37 +0100

    started new challenge, difficulty medium

---
+++ maya.rb
@@ -0,0 +1,21 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+l, h = gets.split.map { |x| x.to_i }
+h.times do
+  numeral = gets.chomp
+end
+s1 = gets.to_i
+s1.times do
+  num_1line = gets.chomp
+end
+s2 = gets.to_i
+s2.times do
+  num_2line = gets.chomp
+end
+operation = gets.chomp
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "result"
